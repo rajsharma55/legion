ae54b5372102c087a3454306bb0e1b24ef0bd026
Nick <nikhilnick00007@gmail.com>
2020-09-20 11:18:11 +0000

    Revert "rolex: Fix QuickSwitch support"
    This reverts commit 6c8a3cd8f7bd490dff8489214fa73d6803808e4f.

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Properties
 -include $(DEVICE_PATH)/vendor_prop.mk
 
